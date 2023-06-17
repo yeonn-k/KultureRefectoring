09070179e043d612b187ac8146bf49a23452c0f5
minjeong <hn01472@gmail.com>
2023-06-17 20:21:23 +0900

    🐣ADD: footer UI 구현

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 1440px;
+  height: 100px;
+  background-color: #121212;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+
+export const FooterImg = styled.img`
+  width: 100px;
+`;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { FooterContainer, FooterImg } from './Footer';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer>
+      <FooterImg src="../images/common/kulture-logo-mono.png" />
+    </FooterContainer>
+  );
 };
 
 export default Footer;
