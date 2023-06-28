5581c609963bdb9b08beab61a4bd8cf98d690d94
KIM_MS <78401083+sstaar91@users.noreply.github.com>
2023-06-28 19:06:12 +0900

    config conflict 해결

--- src/config.js
+++ src/config.js
@@ -5,14 +5,8 @@ export const APIS = {
   bid: `${BASE_URL_K}/bid`,
   order: `${BASE_URL_K}/order`,
   review: `${BASE_URL_K}/review`,
-<<<<<<< HEAD
-  users: `${BASE_URL_K}/users`,
-  events: `${BASE_URL_K}/events`,
-  wishlist: `${BASE_URL_K}/wishlist`,
-=======
   users: `${BASE_URL_H}/users`,
   events: `${BASE_URL_H}/events`,
   wishlist: `${BASE_URL_H}/wishlist`,
->>>>>>> d6009fe (🐥 MODIFY: 상단이동버튼 css수정, 쿼리 수정, token 필터 범위 수정)
   payment: `${BASE_URL_K}/payment`,
 };
